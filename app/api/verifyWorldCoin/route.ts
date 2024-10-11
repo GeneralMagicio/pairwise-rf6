f579cbc6f5c167ae7026c692141173caa8a588ff
HrithikSampson <hrithikedwardsampson@gmail.com>
2024-10-11 18:06:34 +0530

    remove default in the POST request

--- app/api/verifyWorldCoin/route.ts
+++ app/api/verifyWorldCoin/route.ts
@@ -2,7 +2,7 @@ import { verifyCloudProof, IVerifyResponse } from "@worldcoin/idkit";
 import { actionId, appId } from "@/app/lib/constants";
 import { NextRequest, NextResponse } from "next/server";
 
-export default async function POST(req: NextRequest) {
+export async function POST(req: NextRequest) {
   console.log(req);
   const { proof } = await req.json();
   try {
