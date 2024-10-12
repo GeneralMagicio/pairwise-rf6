a08d39435cc1db9a7010c8de0cf035729fdf1ff6
Mahdi <mahdighajary@gmail.com>
2024-10-12 18:15:38 +0330

    More improvements to category allocation

--- app/allocation/components/CategoryAllocation.tsx
+++ app/allocation/components/CategoryAllocation.tsx
@@ -1,18 +1,22 @@
+import debounce from 'lodash.debounce';
 import Image from 'next/image';
-import { FC } from 'react';
+import { ChangeEventHandler, FC, useEffect, useRef } from 'react';
 
 export interface Category {
-  imageSrc: string;
-  title: string;
-  description: string;
-  projectCount: number;
+  id: number
+  imageSrc: string
+  title: string
+  description: string
+  projectCount: number
 }
 
 interface CategoryAllocationProps extends Category {
-  allocationPercentage: number;
-  onDelegate: () => void;
-  onScore: () => void;
-  onLockClick: () => void;
+  allocationPercentage: number
+  onDelegate: () => void
+  onScore: () => void
+  onLockClick: () => void
+  locked: boolean
+  onPercentageChange: (value: number) => void
 }
 
 const CategoryAllocation: FC<CategoryAllocationProps> = ({
@@ -24,47 +28,102 @@ const CategoryAllocation: FC<CategoryAllocationProps> = ({
   onDelegate,
   onScore,
   onLockClick,
+  locked,
+  onPercentageChange,
 }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = debounce((event) => {
+    const value = event.target.value;
+    onPercentageChange(Number(value));
+  }, 1500);
+
+  useEffect(() => {
+    if (inputRef.current?.value) {
+      inputRef.current.value = `${allocationPercentage}`;
+    }
+  }, [allocationPercentage]);
+
   return (
-    <div className="border rounded-lg p-4 flex justify-between">
+    <div className="flex justify-between rounded-lg border p-4">
       <div className="flex max-w-[75%] space-x-4">
         <div className=" rounded-lg">
-          <Image src={imageSrc} alt={title} width={96} height={96}/>
+          <Image src={imageSrc} alt={title} width={96} height={96} />
         </div>
-        <div className='space-y-2 max-w-[70%]'>
-          <h3 className="font-semibold text-lg">{title}</h3>
+        <div className="max-w-[70%] space-y-2">
+          <h3 className="text-lg font-semibold">{title}</h3>
           <p className="text-sm text-gray-600">{description}</p>
-          <p className="text-xs rounded-lg text-gray-800 mt-1 w-fit p-2 bg-gray-100">{projectCount} projects</p>
+          <p className="mt-1 w-fit rounded-lg bg-gray-100 p-2 text-xs text-gray-800">
+            {projectCount}
+            {' '}
+            projects
+          </p>
         </div>
       </div>
       <div className="flex w-fit flex-col gap-2">
         <div className="flex items-center space-x-2">
-          <div className='flex justify-center gap-6 border px-10 py-2'>
-            <button className="text-gray-500 font-bold">-</button>
-            <div className="bg-white rounded">
-              <span className="font-semibold">{allocationPercentage.toFixed(1)}%</span>
+          <div className="flex justify-center gap-6 border px-10 py-2">
+            <button className="font-bold text-gray-500">-</button>
+            <div className="flex gap-1 rounded bg-white">
+              <input
+                onChange={handleInputChange}
+                ref={inputRef}
+                className="w-16 text-center font-semibold"
+                type="number"
+                defaultValue={allocationPercentage}
+              />
+              <span> % </span>
             </div>
-            <button className="text-gray-500 font-bold">+</button>
+            <button className="font-bold text-gray-500">+</button>
           </div>
           <button onClick={onLockClick} className="text-gray-500">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
-            </svg>
+            {locked
+              ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0
+                     01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )
+              : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-5"
+                    viewBox="0 0 20 20"
+                    fill="red"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0
+                     01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
           </button>
         </div>
-        <div className="bg-gray-50 border text-gray-500 w-fit text-sm px-[53px] py-2">
-          {(allocationPercentage * 100000).toLocaleString()} OP
+        <div className="w-fit border bg-gray-50 px-[53px] py-2 text-sm text-gray-500">
+          {(allocationPercentage * 100000).toLocaleString()}
+          {' '}
+          OP
         </div>
-        <div className='flex gap-2 my-2'>
+        <div className="my-2 flex gap-2">
           <button
             onClick={onScore}
-            className="bg-primary text-white px-4 py-2 rounded-md text-sm whitespace-nowrap font-medium"
+            className="whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-medium text-white"
           >
             Score Projects
           </button>
           <button
             onClick={onDelegate}
-            className="text-gray-700 px-4 py-2 border rounded-md text-sm font-medium"
+            className="rounded-md border px-4 py-2 text-sm font-medium text-gray-700"
           >
             Delegate
           </button>
--- app/allocation/utils.ts
+++ app/allocation/utils.ts
@@ -1,36 +1,43 @@
-interface RankItem {
-  id: number;
-  percentage: number;
-  locked: boolean;
+export interface RankItem {
+  id: number
+  percentage: number
+  locked: boolean
 }
 
 const roundFractions = (value: number, fractions: number) => {
-  return Math.round(value * Math.pow(10, fractions)) / Math.pow(10, fractions)
-}
+  return Math.round(value * Math.pow(10, fractions)) / Math.pow(10, fractions);
+};
 
-const modifyPercentage = <T extends RankItem>(values: T[], newValue: T) : T[] => {
+export const modifyPercentage = <T extends RankItem>(values: T[], newValue: T): T[] => {
   // const totalPercentage = 100
 
-  const currIndex = values.findIndex((el) => el.id === newValue.id)
+  const currIndex = values.findIndex(el => el.id === newValue.id);
 
-  if (currIndex === -1) throw new Error ("New value id not found");
+  if (currIndex === -1) throw new Error ('New value id not found');
 
-  const newValueDifference = newValue.percentage - values[currIndex].percentage
+  console.log(values, newValue);
+
+  const newValueDifference = newValue.percentage - values[currIndex].percentage;
 
   const restSum = values.reduce((acc, curr) => {
-    if (curr.id === newValue.id || curr.locked) return acc
-    else return acc + curr.percentage
-  }, 0)
+    if (curr.id === newValue.id || curr.locked) return acc;
+    else return acc + curr.percentage;
+  }, 0);
 
-  if (Math.abs(newValueDifference) > restSum) throw new Error("Modification not possible")
+  if (newValueDifference > restSum) throw new Error('Modification not possible');
 
-  return values.map((item) => {
-    if (item.id === newValue.id) return newValue
-    if (item.locked) return item
+  const result = values.map((item) => {
+    if (item.id === newValue.id) return newValue;
+    if (item.locked) return item;
     else return {
       ...item,
-      percentage: roundFractions(item.percentage + (-1 * newValueDifference * item.percentage / restSum), 2)
-    }
+      percentage: roundFractions(item.percentage + (-1 * newValueDifference * item.percentage / restSum), 2),
+    };
+  });
 
-  })
-}
+  const sum = result.reduce((acc, curr) => acc += curr.percentage, 0);
+
+  if (sum > 100.1) throw new Error('Bigger than 100 error');
+
+  return result;
+};
