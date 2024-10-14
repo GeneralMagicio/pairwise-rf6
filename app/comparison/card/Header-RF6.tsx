2a419a8217585833cf24b5234c669fbd15ed8a30
Mahdi <mahdighajary@gmail.com>
2024-10-14 15:45:52 +0330

    Import image from next

--- app/comparison/card/Header-RF6.tsx
+++ app/comparison/card/Header-RF6.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import { ConnectButton } from '@/app/utils/wallet/Connect';
 import { PwLogo } from '@/public/assets/icon-components/PairwiseLogo';
 
