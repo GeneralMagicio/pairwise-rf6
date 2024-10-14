d8d4960263690c2de9874a164f91c7fbd87c0304
HrithikSampson <hrithikedwardsampson@gmail.com>
2024-10-14 17:58:11 +0530

    refining the farcaster siginin success modal

--- app/allocation/components/FarcasterModal.tsx
+++ app/allocation/components/FarcasterModal.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Modal from "@/app/utils/Modal";
 import { useSignIn, QRCode } from '@farcaster/auth-kit';
 import Image from "next/image"
+import styles from '../../styles/Spinner.module.css';
 
 interface FarcasterModalProps {
     isOpen: boolean;
@@ -20,7 +21,9 @@ const FarcasterModal: React.FC<FarcasterModalProps> = ({ isOpen, onClose }) => {
         onSuccess: ({ fid }) => console.log('Your fid:', fid),
     });
 
+    const [delegates,setDelegateAmount] = useState<{username: string, points: Number}[] | undefined>(undefined);
     const [isLoading, setIsLoading] = useState(false);
+    console.log(isSuccess)
 
     useEffect(() => {
         if (isOpen) {
@@ -34,6 +37,22 @@ const FarcasterModal: React.FC<FarcasterModalProps> = ({ isOpen, onClose }) => {
         }
     }, [isConnected, signIn]);
 
+    useEffect(()=>{
+        if (data && data.username) {
+                setDelegateAmount([
+                    {
+                        username: "username1",
+                        points: 200,
+                    },
+                    {
+                        username: "username2",
+                        points: 300,
+                    }
+                ])
+                setIsLoading(true);
+        }
+    }, [isSuccess])
+
     const handleCopyLink = async () => {
         try {
             if (url) await navigator.clipboard.writeText(url);
@@ -41,14 +60,16 @@ const FarcasterModal: React.FC<FarcasterModalProps> = ({ isOpen, onClose }) => {
             console.error('Failed to copy: ', err);
         }
     };
-
     useEffect(() => {
         if (data && data.username) {
-            setTimeout(() => {
-                setIsLoading(true);
-            }, 2000);
+            setIsLoading(false);
+            setDelegateAmount([
+                { username: "username1", points: 200 },
+                { username: "username2", points: 300 }
+            ]);
+            setIsLoading(true);
         }
-    }, [data]);
+    }, [data?.username]);
 
     return (
         <Modal isOpen={isOpen} onClose={onClose}>
@@ -82,11 +103,49 @@ const FarcasterModal: React.FC<FarcasterModalProps> = ({ isOpen, onClose }) => {
             )}
             {isSuccess && data?.username && (
                 isLoading?
-                <div>
-
-                </div>
+                (<div className="flex flex-col w-full items-center justify-center gap-6 px-4 py-6">
+                    
+                    <div className="flex flex-col w-full items-center justify-center gap-2">
+                        <div className="flex items-center justify-center">
+                            <div className={styles.spinner}></div>
+                        </div>
+                        <div className="text-lg text-[#05060B]">Looking for Voting Power on Farcaster</div>
+                        <div className="text-[#636779] font-normal text-sm tex-wrap text-center">Searching Farcaster for people who delegated voting power to you.</div>
+                    </div>
+                </div>)
                 :
-                <p></p>
+                (<div>
+                    {delegates && delegates.length==0 && (<div className="flex flex-col w-full items-center justify-center gap-6 px-4 py-10">
+                        <Image src="assets/images/world-star-success.svg" width={100} height={100} alt=""/>
+                        <div className="gap-2 flex flex-col items-center">
+                            <div className="text-sm text-[#232634]">
+                                <div>Successfully connected to your Farcaster account</div>
+                                <div>@{data.username}</div>
+                            </div>
+                            <p className="font-semibold text-mxl text-wrap">You currently don’t have any delegations to your Farcaster account</p>
+                            <p className="text-[#636779] text-sm text-wrap">You can always check back later if someone has delegated you voting power</p>
+                        </div>
+                        <button className="bg-[#FF0420] w-full text-white py-2.5 px-5 rounded-lg" onClick={onClose}><p className="p-0.5">Done</p></button>
+                    </div>)}
+                    {delegates && delegates.length>0 && (<div className="flex flex-col w-full items-center justify-center gap-6 px-4 py-10">
+                        <Image src="assets/images/star-old.svg" width={100} height={100} alt=""/>
+                        <div className="gap-2 flex flex-col items-center">
+                            <div className="text-sm text-[#232634]">
+                                <div>Successfully connected to your Farcaster account</div>
+                                <div>@{data.username}</div>
+                            </div>
+                            <p className="font-semibold text-mxl text-wrap">You have been delegated voting power from the users below</p>
+                            {delegates.map(({username,points})=><div className="text-center w-full gap-1 text-[#636779] text-md text-wrap">
+                                <span className="text-sm text-[#FF0420]">{points.toString()}</span> from @{username}
+                            </div>)}
+                        </div>
+                        <div className="text-[#FF0420] font-semibold w-full text-center">
+                            <div className="text-base ">Your current voting power</div>
+                            <div className="text-5xl font-bold text-[#FF0420] p-3">{20000}</div>
+                        </div>
+                        <button className="bg-[#FF0420] w-full text-white py-2.5 px-5 rounded-lg" onClick={onClose}><p className="p-0.5">Done</p></button>
+                    </div>)}
+                </div>)
             )}
         </Modal>
     );
