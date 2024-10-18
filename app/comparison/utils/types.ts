5badbd3365f677fc82314554288d88ed8719ae67
Meriem-BM <barhoumi.meriem1@gmail.com>
2024-10-18 03:02:29 +0100

    chore: linting errors

--- app/comparison/utils/types.ts
+++ app/comparison/utils/types.ts
@@ -922,13 +922,13 @@ export type ProjectMetadata =
           fundingRound: string
         }
         | {
-            grant: string
-            link: string
-            amount: string
-            date: string
-            details: string
-            fundingRound: null
-          }
+          grant: string
+          link: string
+          amount: string
+          date: string
+          details: string
+          fundingRound: null
+        }
       )[]
     }
     pricingModel: {
@@ -1064,201 +1064,72 @@ export type ProjectMetadata =
     testimonials: string
   }
   | {
-      id: string
-      applicationId: string
-      projectId: string
-      category: string
-      applicationCategory: string
-      organization: {
-        name: string
-        description: string
-        organizationAvatarUrl: string
-        organizationCoverImageUrl: string
-        socialLinks: {
-          website: string[]
-          farcaster: string[]
-          twitter: string
-          mirror: string
-        }
-        team: string[]
-      }
+    id: string
+    applicationId: string
+    projectId: string
+    category: string
+    applicationCategory: string
+    organization: {
       name: string
       description: string
-      profileAvatarUrl: string
-      projectCoverImageUrl: string
+      organizationAvatarUrl: string
+      organizationCoverImageUrl: string
       socialLinks: {
         website: string[]
         farcaster: string[]
         twitter: string
         mirror: string
       }
-      team: {
-        fid: number
-        object: string
-        pfp_url: string
-        profile: {
-          bio: {
-            text: string
-          }
+      team: string[]
+    }
+    name: string
+    description: string
+    profileAvatarUrl: string
+    projectCoverImageUrl: string
+    socialLinks: {
+      website: string[]
+      farcaster: string[]
+      twitter: string
+      mirror: string
+    }
+    team: {
+      fid: number
+      object: string
+      pfp_url: string
+      profile: {
+        bio: {
+          text: string
         }
-        username: string
-        power_badge: boolean
-        display_name: string
-        active_status: string
-        verifications: string[]
-        follower_count: number
-        custody_address: string
-        following_count: number
-        verified_addresses: {
-          eth_addresses: string[]
-          sol_addresses: null
-        }
-      }[]
-      github: (
+      }
+      username: string
+      power_badge: boolean
+      display_name: string
+      active_status: string
+      verifications: string[]
+      follower_count: number
+      custody_address: string
+      following_count: number
+      verified_addresses: {
+        eth_addresses: string[]
+        sol_addresses: null
+      }
+    }[]
+    github: (
       | {
-          id: string
-          type: string
-          url: string
-          name: null
-          description: null
-          verified: boolean
-          openSource: boolean
-          containsContracts: boolean
-          createdAt: string
-          updatedAt: string
-          projectId: string
-          metrics?: undefined
-        }
-      | {
-          id: string
-          type: string
-          url: string
-          name: string
-          description: string
-          verified: boolean
-          openSource: boolean
-          containsContracts: boolean
-          createdAt: string
-          updatedAt: string
-          projectId: string
-          metrics: {
-            artifact_url: string
-            num_contributors: string
-            num_trusted_contributors: string
-            num_contributors_last_6_months: string
-            num_stars: string
-            num_trusted_stars: string
-            trust_weighted_stars: string
-            num_forks: string
-            num_trusted_forks: string
-            trust_weighted_forks: string
-            trust_rank_for_repo_in_category: string
-            age_of_project_years: string
-            license: string
-          }
-        }
-      )[]
-      packages: {
+        id: string
+        type: string
         url: string
         name: null
         description: null
-      }[]
-      links: {
-        url: string
-        name: string
-        description: string
-      }[]
-      contracts: null
-      grantsAndFunding: {
-        investments: null
-        revenue: null
-        grants: (
-        | {
-            grant: string
-            link: null
-            amount: string
-            date: string
-            details: null
-            fundingRound: string
-          }
-        | {
-            grant: string
-            link: string
-            amount: string
-            date: string
-            details: string
-            fundingRound: null
-          }
-        )[]
+        verified: boolean
+        openSource: boolean
+        containsContracts: boolean
+        createdAt: string
+        updatedAt: string
+        projectId: string
+        metrics?: undefined
       }
-      pricingModel: {
-        type: string
-        details: string
-      }
-      impactStatement: {
-        category: string
-        subcategory: string[]
-        statement: {
-          create: {
-            answer: string
-            question: string
-          }[]
-        }
-      }
-      testimonials: string
-    }
-  | {
-      id: string
-      applicationId: string
-      projectId: string
-      category: string
-      applicationCategory: string
-      organization: {
-        name: string
-        description: string
-        organizationAvatarUrl: string
-        organizationCoverImageUrl: string
-        socialLinks: {
-          website: null
-          farcaster: null
-          twitter: string
-          mirror: string
-        }
-        team: string[]
-      }
-      name: string
-      description: string
-      profileAvatarUrl: string
-      projectCoverImageUrl: string
-      socialLinks: {
-        website: null
-        farcaster: null
-        twitter: null
-        mirror: null
-      }
-      team: {
-        fid: number
-        object: string
-        pfp_url: string
-        profile: {
-          bio: {
-            text: string
-          }
-        }
-        username: string
-        power_badge: boolean
-        display_name: string
-        active_status: string
-        verifications: null
-        follower_count: number
-        custody_address: string
-        following_count: number
-        verified_addresses: {
-          eth_addresses: null
-          sol_addresses: null
-        }
-      }[]
-      github: {
+      | {
         id: string
         type: string
         url: string
@@ -1285,45 +1156,174 @@ export type ProjectMetadata =
           age_of_project_years: string
           license: string
         }
-      }[]
-      packages: null
-      links: {
-        url: string
-        name: string
-        description: string
-      }[]
-      contracts: {
-        address: string
-        deploymentTxHash: string
-        deployerAddress: string
-        verificationProof: string
-        chainId: number
-      }[]
-      grantsAndFunding: {
-        investments: {
+      }
+    )[]
+    packages: {
+      url: string
+      name: null
+      description: null
+    }[]
+    links: {
+      url: string
+      name: string
+      description: string
+    }[]
+    contracts: null
+    grantsAndFunding: {
+      investments: null
+      revenue: null
+      grants: (
+        | {
+          grant: string
+          link: null
           amount: string
-          year: string
+          date: string
+          details: null
+          fundingRound: string
+        }
+        | {
+          grant: string
+          link: string
+          amount: string
+          date: string
           details: string
+          fundingRound: null
+        }
+      )[]
+    }
+    pricingModel: {
+      type: string
+      details: string
+    }
+    impactStatement: {
+      category: string
+      subcategory: string[]
+      statement: {
+        create: {
+          answer: string
+          question: string
         }[]
-        revenue: null
-        grants: null
       }
-      pricingModel: {
-        type: string
-        details: string
+    }
+    testimonials: string
+  }
+  | {
+    id: string
+    applicationId: string
+    projectId: string
+    category: string
+    applicationCategory: string
+    organization: {
+      name: string
+      description: string
+      organizationAvatarUrl: string
+      organizationCoverImageUrl: string
+      socialLinks: {
+        website: null
+        farcaster: null
+        twitter: string
+        mirror: string
       }
-      impactStatement: {
-        category: string
-        subcategory: string[]
-        statement: {
-          create: {
-            answer: string
-            question: string
-          }[]
+      team: string[]
+    }
+    name: string
+    description: string
+    profileAvatarUrl: string
+    projectCoverImageUrl: string
+    socialLinks: {
+      website: null
+      farcaster: null
+      twitter: null
+      mirror: null
+    }
+    team: {
+      fid: number
+      object: string
+      pfp_url: string
+      profile: {
+        bio: {
+          text: string
         }
       }
-      testimonials: string
-    };
+      username: string
+      power_badge: boolean
+      display_name: string
+      active_status: string
+      verifications: null
+      follower_count: number
+      custody_address: string
+      following_count: number
+      verified_addresses: {
+        eth_addresses: null
+        sol_addresses: null
+      }
+    }[]
+    github: {
+      id: string
+      type: string
+      url: string
+      name: string
+      description: string
+      verified: boolean
+      openSource: boolean
+      containsContracts: boolean
+      createdAt: string
+      updatedAt: string
+      projectId: string
+      metrics: {
+        artifact_url: string
+        num_contributors: string
+        num_trusted_contributors: string
+        num_contributors_last_6_months: string
+        num_stars: string
+        num_trusted_stars: string
+        trust_weighted_stars: string
+        num_forks: string
+        num_trusted_forks: string
+        trust_weighted_forks: string
+        trust_rank_for_repo_in_category: string
+        age_of_project_years: string
+        license: string
+      }
+    }[]
+    packages: null
+    links: {
+      url: string
+      name: string
+      description: string
+    }[]
+    contracts: {
+      address: string
+      deploymentTxHash: string
+      deployerAddress: string
+      verificationProof: string
+      chainId: number
+    }[]
+    grantsAndFunding: {
+      investments: {
+        amount: string
+        year: string
+        details: string
+      }[]
+      revenue: null
+      grants: null
+    }
+    pricingModel: {
+      type: string
+      details: string
+    }
+    impactStatement: {
+      category: string
+      subcategory: string[]
+      statement: {
+        create: {
+          answer: string
+          question: string
+        }[]
+      }
+    }
+    testimonials: string
+  };
 
 export interface ICategory {
   id: number
