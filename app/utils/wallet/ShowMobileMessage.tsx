724042cd3a9f7eeac13f8788af348a1cdf33c488
HrithikSampson <hrithikedwardsampson@gmail.com>
2024-11-08 19:10:07 +0530

    run linter

--- app/utils/wallet/ShowMobileMessage.tsx
+++ app/utils/wallet/ShowMobileMessage.tsx
@@ -13,20 +13,20 @@ export default function ShowMobileMessage({
     <div className="relative h-screen w-screen">
       {showMobileMessage && (
         <>
-        <div className='absolute w-full aspect-square top-0'>
-          <Image className='object-cover' fill src="/assets/images/mobile-header.svg" alt=""/>
-        </div>
-        <div className={`flex h-full flex-col items-center justify-center gap-4 px-4 ${(showMobileMessage) ? 'block sm:hidden' : 'hidden'}`}>
-          <Image src="/assets/images/star-blonde.svg" width={100} height={120} alt="" />
-          <div className="text-wrap text-center text-xl font-semibold text-dark-500">Change or update your browser for a better experience</div>
-          <div className="text-wrap text-center text-base text-gray-400">Pairwise works best on desktop browsers. For the best experience, please switch to your desktop device.</div>
-          <button
-            className="rounded-lg bg-primary p-6 py-2.5 text-white"
-            onClick={() => setShowMobileMessage(false)}
-          >
-            Ok
-          </button>
-        </div>
+          <div className="absolute top-0 aspect-square w-full">
+            <Image className="object-cover" fill src="/assets/images/mobile-header.svg" alt="" />
+          </div>
+          <div className={`flex h-full flex-col items-center justify-center gap-4 px-4 ${(showMobileMessage) ? 'block sm:hidden' : 'hidden'}`}>
+            <Image src="/assets/images/star-blonde.svg" width={100} height={120} alt="" />
+            <div className="text-wrap text-center text-xl font-semibold text-dark-500">Change or update your browser for a better experience</div>
+            <div className="text-wrap text-center text-base text-gray-400">Pairwise works best on desktop browsers. For the best experience, please switch to your desktop device.</div>
+            <button
+              className="rounded-lg bg-primary p-6 py-2.5 text-white"
+              onClick={() => setShowMobileMessage(false)}
+            >
+              Ok
+            </button>
+          </div>
         </>
       )}
       <div className={showMobileMessage ? 'hidden sm:block' : 'block'}>
