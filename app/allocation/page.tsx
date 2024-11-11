2ec9c95e652d496a69b2626b9f66187fcf60554b
maryjaf <111529185+maryjaf@users.noreply.github.com>
2024-11-11 11:21:17 +0330

    change name of update ballot button
    Update ballot on Optimism

--- app/allocation/page.tsx
+++ app/allocation/page.tsx
@@ -640,7 +640,7 @@ const AllocationPage = () => {
                       || (isBadgeholder && !isBGCategoryVoted())
                     }
                   >
-                    Update Ballot
+                    Update ballot on Optimism
                   </button>
                 )}
           </div>
