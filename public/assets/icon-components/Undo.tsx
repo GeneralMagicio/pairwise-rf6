ac43c7056f46e867c3d813f6e543ff02b69c58ca
HrithikSampson <hrithikedwardsampson@gmail.com>
2024-12-14 21:57:49 +0530

    run linter

--- public/assets/icon-components/Undo.tsx
+++ public/assets/icon-components/Undo.tsx
@@ -10,9 +10,9 @@ export const UndoIcon = () => {
       <path
         d="M7.83332 11.6667L3.66665 7.50004M3.66665 7.50004L7.83332 3.33337M3.66665 7.50004H8.99998C11.8002 7.50004 13.2004 7.50004 14.2699 8.04501C15.2107 8.52437 15.9756 9.28928 16.455 10.2301C17 11.2996 17 12.6998 17 15.5V16.6667"
         stroke="#344054"
-        stroke-width="1.66667"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.66667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
