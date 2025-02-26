bc967abfd585c1dfd1eaf449a0266a6046728592
Meriem-BM <barhoumi.meriem1@gmail.com>
2025-02-26 12:11:00 +0000

    add voted badge condition

--- app/allocation/[category]/page.tsx
+++ app/allocation/[category]/page.tsx
@@ -414,7 +414,7 @@ const RankingPage = () => {
       </Modal>
       <HeaderRF6 />
       <Link
-        className="text-md flex items-center gap-2 px-12 pt-8 text-gray-700"
+        className="flex items-center pt-8 px-12 gap-2 text-md text-gray-700"
         href="/allocation"
       >
         <ArrowLeft2Icon size={16} />
@@ -443,10 +443,19 @@ const RankingPage = () => {
               </p>
             </div>
             <div className="flex items-center gap-2">
-              <div className="flex items-center justify-center gap-2 rounded-2xl border border-voting-border bg-voting-bg px-3 py-1 text-xs text-voting-text">
-                {votingStatusMap[VotingStatus.READY_TO_SUBMIT].text}
-                <CheckIcon size={18} />
-              </div>
+              {attestationLink
+                ? (
+                    <div className="flex items-center justify-center gap-2 rounded-2xl border border-voting-border bg-voting-bg px-3 py-1 text-xs text-voting-text">
+                      {votingStatusMap[VotingStatus.VOTED].text}
+                      <CheckIcon size={18} />
+                    </div>
+                  )
+                : (
+                    <div className="flex items-center justify-center gap-2 rounded-2xl border border-voting-border bg-voting-bg px-3 py-1 text-xs text-voting-text">
+                      {votingStatusMap[VotingStatus.READY_TO_SUBMIT].text}
+                      <CheckIcon size={18} />
+                    </div>
+                  )}
               {attestationLink && (
                 <Link
                   className="flex items-center justify-center gap-2 px-3 py-1 text-xs text-gray-600 underline"
@@ -461,8 +470,8 @@ const RankingPage = () => {
           </div>
           <div className="w-full rounded-lg border border-[#E0F2FE] bg-[#E0F2FE] px-4 py-2">
             <p className="text-sm text-[#026AA2]">
-              Attested on Nov 11 at 3:20 PM. You can make changes and resubmit
-              until Nov 13 at 12:00 AM UTC. To do so, simply edit and submit
+              Attested on Nov 11 at 3:20 PM. You can make changes and resubmit
+              until Nov 13 at 12:00 AM UTC. To do so, simply edit and submit
               again.
             </p>
           </div>
@@ -497,7 +506,7 @@ const RankingPage = () => {
                       onClick={unlockSelection}
                     >
                       <UnlockIcon />
-                      <p className="text-sm text-gray-600">Unlock selection</p>
+                      <p className="text-sm text-gray-600">Unlock allocation</p>
                     </button>
                   )}
                   {isLocked && isUnlocked && (
@@ -509,7 +518,7 @@ const RankingPage = () => {
                       onClick={lockSelection}
                     >
                       <LockIcon />
-                      <p className="text-sm text-gray-600">Lock selection</p>
+                      <p className="text-sm text-gray-600">Lock allocation</p>
                     </button>
                   )}
                 </>
@@ -599,12 +608,7 @@ const RankingPage = () => {
               Back to Categories
             </button>
             <button
-              className={`font-semibold" flex items-center justify-center gap-3 rounded-lg px-10 py-2
-              ${
-    totalShareError
-      ? 'bg-gray-200 text-gray-400'
-      : 'bg-primary text-white'
-    }`}
+              className="flex items-center justify-center gap-3 rounded-lg bg-primary px-10 py-2 font-semibold text-white disabled:bg-gray-200 disabled:text-gray-400"
               onClick={submitVotes}
               disabled={!!totalShareError}
             >
